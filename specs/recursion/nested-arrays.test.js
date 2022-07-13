22a3756807c19dd71d3df3352fc1684a9358247e
Aaron Davis <aaron@ripemetrics.com>
2022-07-13 08:51:43 -0700

    Nested Arrays

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -1,15 +1,4 @@
-/*
-
-  write a function that accepts an array that only contains
-  two types of things: numbers and arrays. those nested arrays
-  also only contain numbers and other, nested arrays.
-
-  example: nestedAdd([1, 2, [3]]) = 6
-           nestedAdd([[[2]], 1, [1, 3]]) = 7
-
- */
-
-    function nestedAdd(array) {
+function nestedAdd(array) {
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
         const current = array[i];
@@ -22,6 +11,16 @@
     return sum;
     }
 
+/*
+
+  write a function that accepts an array that only contains
+  two types of things: numbers and arrays. those nested arrays
+  also only contain numbers and other, nested arrays.
+
+  example: nestedAdd([1, 2, [3]]) = 6
+           nestedAdd([[[2]], 1, [1, 3]]) = 7
+
+ */
 test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
