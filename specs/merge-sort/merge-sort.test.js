2eafe5504c7f3f6db5044d7a7093542d6aace3c3
Aaron Davis <aaron@ripemetrics.com>
2022-07-13 08:29:04 -0700

    Recursion Sort, Nested Array and Factorial, Merge Sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,50 @@
   You'll need to write more than just one function
 */
 
+
 const mergeSort = (nums) => {
-  // code goes here
-};
+    // base case - if nums length is 0 or 1
+    if (nums.length < 2) {
+        return nums;
+    }
+
+    const length = nums.length;
+
+    // floor rounds down so if middle if lengeth is 7, rounds down to 3
+    const middle = Math.floor(length / 2);
+
+    // the first number in the array to the middle of the array
+    const left = nums.slice(0, middle);
+    //
+    // after the middle of the array
+    const right = nums.slice(middle);
+
+    // merge takes two sorted lists and returns one sorted list
+    return merge(mergeSort(left), mergeSort(right));
+    };
+
+    const merge = (left, right) => {
+    const results = [];
+
+    // go until one list runs outs
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+        // shift removes the first element in an array and returns it
+        // it's like .pop() for the front
+        results.push(left.shift());
+        } else {
+        results.push(right.shift());
+        }
+    }
+
+    // either left or right will be empty so you can safely concat both
+    return results.concat(left, right);
+    };
+
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
